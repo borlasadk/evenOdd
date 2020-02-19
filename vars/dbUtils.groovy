d65c3e1714c437c6645f78d531c439a485ba8849
dauren <dauren_altayev@epam.com>
2020-02-19 14:24:20 +0300

    Test Script#8

--- vars/dbUtils.groovy
+++ vars/dbUtils.groovy
@@ -2,8 +2,8 @@
 
 def createReport(
         String ORACLE_TNS,
-        String CI_DB_PMC_SYSTEM_PASSW
+        String SECRET
 ) {
 //    return exec("cd Scripts\\difference_db\\ && difference_db.cmd ${ORACLE_TNS} ${CI_DB_PMC_SYSTEM_PASSW}", true).trim().split()[-1]
-    exec("cd Scripts\\difference_db\\ && difference_db.cmd ${ORACLE_TNS} ${CI_DB_PMC_SYSTEM_PASSW}")
+    exec("cd Scripts\\difference_db\\ && difference_db.cmd ${ORACLE_TNS} ${SECRET}")
 }
