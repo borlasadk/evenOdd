32e34fef89b4e3fe3bed1a785fa90302bdd43543
dauren <dauren_altayev@epam.com>
2020-02-20 12:31:29 +0300

    Test Script#38

--- Scripts/difference_db/difference_db.cmd
+++ Scripts/difference_db/difference_db.cmd
@@ -29,7 +29,6 @@ for /F "usebackq tokens=* delims=" %%i In ("version.properties") do set _CTC_DB_
 
 echo on
 sqlplus %_SCHEMA%/%_PASSW%@%_TNS_NAME% @difference_db.sql
-rem _CTC_DB_VERSION
-echo _CTC_DB_VERSION
+
 @del version.properties on 2>nul
 
