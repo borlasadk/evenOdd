39bdd899c9f25866d711e220a6a8fe3a6600bc9c
dauren <dauren_altayev@epam.com>
2020-02-20 13:05:05 +0300

    Test Script#46

--- vars/dbUtils.groovy
+++ vars/dbUtils.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def VersionReport () {
-    return dbUtils.createReport()
+    return dbUtils.createReport(ORACLE_TNS, CI_DB_PMC_SYSTEM_PASSW)
 //    return exec("cd Scripts\\difference_db\\ && difference_db.cmd ${ORACLE_TNS} ${CI_DB_PMC_SYSTEM_PASSW}", true).trim().split()[-1]
 }
 
