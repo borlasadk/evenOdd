af03719416a300a671c2ea1a9a67374e3c1c9be8
dauren <dauren_altayev@epam.com>
2020-02-20 13:08:36 +0300

    Test Script#48

--- vars/dbUtils.groovy
+++ vars/dbUtils.groovy
@@ -1,14 +1,10 @@
 #!/usr/bin/env groovy
 
-def VersionReport () {
-    return dbUtils.createReport(ORACLE_TNS, CI_DB_PMC_SYSTEM_PASSW)
-//    return exec("cd Scripts\\difference_db\\ && difference_db.cmd ${ORACLE_TNS} ${CI_DB_PMC_SYSTEM_PASSW}", true).trim().split()[-1]
-}
-
 def createReport(
         String ORACLE_TNS,
         String CI_DB_PMC_SYSTEM_PASSW
 ) {
-    exec("cd Scripts\\difference_db\\ && difference_db.cmd ${ORACLE_TNS} ${CI_DB_PMC_SYSTEM_PASSW}")
-
+    return exec("cd Scripts\\difference_db\\ && difference_db.cmd ${ORACLE_TNS} ${CI_DB_PMC_SYSTEM_PASSW}", true).trim().split()[-1]
 }
+
+
