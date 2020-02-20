9bfe7f80390d41ba613b59acf48e64cc78455a0d
dauren <dauren_altayev@epam.com>
2020-02-20 14:18:48 +0300

    Test Script#53

--- vars/dbUtils.groovy
+++ vars/dbUtils.groovy
@@ -2,7 +2,7 @@
 
 def createReport(
         String ORACLE_TNS,
-        String CI_DB_PMC_SYSTEM_PASSW,
+        String CI_DB_PMC_SYSTEM_PASSW
 
 ) {
      exec("cd Scripts\\difference_db\\ && difference_db.cmd ${ORACLE_TNS} ${CI_DB_PMC_SYSTEM_PASSW}")
