d81fd78906208a09f1c4d1503bdea4fb240fcfdb
dauren <dauren_altayev@epam.com>
2020-02-24 15:48:04 +0300

    Test Mail#2

--- vars/mailNotify.groovy
+++ vars/mailNotify.groovy
@@ -8,7 +8,7 @@ def call(
 ) {
     mail     recipientProviders: [[$class: 'RequesterRecipientProvider']],
              to: Properties.Emails.EMAIL_TEST,
-             cc: Properties.Emails.EMAIL_TEST,
+             replyTo: Properties.Emails.EMAIL_TEST,
              subject: "Report of CTC DB structure changes: ${dbUtils.versionGet()}",
              body:  '''Dear Colleagues, \n\nPlease find attached difference of database structure for coming CTC release 19.11.\n\nThank you.''',
              attachmentsPattern: "${Properties.Paths.DIFFERENCE_DB_DIR}*.txt"
