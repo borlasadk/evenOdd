ec03f1eb80c3eb33a4113a12afd0ecdbe93c5fe0
dauren <dauren_altayev@epam.com>
2020-02-24 17:12:20 +0300

    Test Mail#9

--- vars/mailNotify.groovy
+++ vars/mailNotify.groovy
@@ -9,7 +9,7 @@ def call(
     emailext subject: "Report of CTC DB structure changes: ${dbUtils.versionGet()}",
              body:  "Dear Colleagues, \n\nPlease find attached difference of database structure for coming CTC release 19.11.\n\nThank you.",
              attachmentsPattern: "${Properties.Paths.DIFFERENCE_DB_DIR}*.txt",
-             to: "bcc:${Properties.Emails.EMAIL_TEST2}", Properties.Emails.EMAIL_TEST,
+             to: "bcc:${Properties.Emails.EMAIL_TEST2}", "${Properties.Emails.EMAIL_TEST}",
              recipientProviders: [[$class: 'RequesterRecipientProvider']]
 }
 
